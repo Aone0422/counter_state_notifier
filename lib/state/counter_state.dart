569675cf8f2668b1203e9ac22484a84536daa932
Aone0422 <hasegawa.eiichi.gw1@is.naist.jp>
2020-08-13 20:04:58 +0900

    Add: カウントアプリの実装

---
+++ lib/state/counter_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+part 'counter_state.freezed.dart';
+
+@freezed
+abstract class CounterState with _$CounterState {
+  const factory CounterState({
+    @Default(0) int count,
+  }) = _CounterState;
+}
+
+class CounterStateNotifier extends StateNotifier<CounterState> {
+  CounterStateNotifier() : super(const CounterState());
+  increment() => state = state.copyWith(count: state.count + 1);
+}
